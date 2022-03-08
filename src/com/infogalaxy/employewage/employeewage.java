7dc56d4a6f02440ee2e00eb89125c071f22a4e3c
SuryawanshiRahul <suryawanshirahul9623@gmail.com>
2022-03-08 14:35:06 +0530

    MODIFIED:Calculate Daily Wage Of Employee

--- src/com/infogalaxy/employewage/employeewage.java
+++ src/com/infogalaxy/employewage/employeewage.java
@@ -6,15 +6,21 @@ public class employeewage {
     public static void main(String[] args) {
         //Displaying Welcome to Employee Wage Computation
         System.out.println("welcome to Employee Wage calculation problem ");
+        //UC-2 calculate daily Wage
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY_HR =8;
+        int total_wage = 0;
         Random random = new Random();
         // Uc- 1 Check Employee is Absent Ro Present
         int empAtenddence = random.nextInt()%2;
         System.out.println(empAtenddence);
         if(empAtenddence == 0){
             System.out.println("Employee is Absent");
+            total_wage = 0;
         }else{
             System.out.println("Employee is present");
+            total_wage =WAGE_PER_HR * FULL_DAY_HR;
         }
-
+        System.out.println("Employee A Full Day Wage="+total_wage);
     }
 }
