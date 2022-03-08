97b764d1df21de6c801f2f4b63ff168b3d79353c
SuryawanshiRahul <suryawanshirahul9623@gmail.com>
2022-03-08 15:13:37 +0530

    MODIFIED: Checking For Employee Present Or Absent

--- src/com/infogalaxy/employewage/employeewage.java
+++ src/com/infogalaxy/employewage/employeewage.java
@@ -8,19 +8,28 @@ public class employeewage {
         System.out.println("welcome to Employee Wage calculation problem ");
         //UC-2 calculate daily Wage
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR =8;
+        final int FULL_TIME_HR = 8;
+        final int PART_TIME_HR = 4;
         int total_wage = 0;
         Random random = new Random();
-        // Uc- 1 Check Employee is Absent Ro Present
+        //UC-3-checking Employee Absent Or Present
+        int emptype = random.nextInt()%2;
+        // UC- 1 Check Employee is Absent Ro Present
         int empAtenddence = random.nextInt()%2;
         System.out.println(empAtenddence);
         if(empAtenddence == 0){
             System.out.println("Employee is Absent");
-            total_wage = 0;
         }else{
             System.out.println("Employee is present");
-            total_wage =WAGE_PER_HR * FULL_DAY_HR;
+            System.out.println(emptype);
+            if(emptype == 0){
+                System.out.println("Part Time Employee");
+                total_wage =WAGE_PER_HR * PART_TIME_HR;
+            }else {
+                System.out.println("Full Time Employee");
+                total_wage =WAGE_PER_HR * FULL_TIME_HR;
+            }
         }
-        System.out.println("Employee A Full Day Wage="+total_wage);
+        System.out.println("Employee  Wage="+total_wage);
     }
 }
